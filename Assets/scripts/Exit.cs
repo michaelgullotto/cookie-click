46e58a86553664d1e944e94aa2698b0c3db2220a
michaelgullotto <michael2.g@icloud.com>
2021-03-10 13:06:09 +1100

    updated 10/3
    added exit and built game run as intended

---
+++ Assets/scripts/Exit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ExitToDesktop()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
+    }
+}
